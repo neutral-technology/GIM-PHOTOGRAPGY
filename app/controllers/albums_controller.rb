8da9ee3aea7454cfff99fdaf8d497f4c21fd457b
joskalenda <joskalenda3@gmail.com>
2025-08-26 14:10:04 +0200

    create controller album

---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,76 @@
+class AlbumsController < ApplicationController
+  layout 'default' # applies to all actions
+  before_action :authenticate_user!, except: %i[index]
+  before_action :set_album, only: %i[ show edit update destroy generate_password]
+
+  def index
+    @albums = Album.all.includes(:client).order(created_at: :desc)
+  end
+
+  def new
+    @album = current_user.albums.new
+    @clients = current_user.clients
+  end
+
+  def create
+    @album = current_user.albums.new(album_params)
+    
+    # Automatically generate a password if one wasn't provided
+    generated_password = album_params[:password].presence || SecureRandom.hex(4)
+    @album.password = generated_password
+
+    if @album.save
+      redirect_to @album, notice: 'Album was successfully created.'
+    else
+      @clients = current_user.clients
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  def show
+    if user_signed_in?
+      @image = @album.images.new # Used for the upload form
+      @images = @album.images.with_attached_photo.order(created_at: :desc)
+    else
+      redirect_to new_user_session_path, notice: 'reservé à GIM, connectez-vous pour y acceder'
+    end
+  end
+
+  def edit
+    @clients = current_user.clients
+  end
+
+  def update
+    if @album.update(album_params)
+      redirect_to @album, notice: 'Album was successfully updated.'
+    else
+      @clients = current_user.clients
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @album.destroy
+    redirect_to albums_url, notice: 'Album was successfully destroyed.'
+  end
+
+  def generate_password
+    generated_password = SecureRandom.hex(4)
+    if @album.update(password: generated_password)
+      redirect_to @album, notice: "#{generated_password}"
+    else
+      redirect_to @album, alert: "Failed to generate a new password."
+    end
+  end
+
+  private
+  
+  def set_album
+    @album = Album.friendly.find(params[:id])
+  end
+
+  def album_params
+    params.require(:album).permit(:name, :password, :client_id, :cover_photo)
+  end
+end
