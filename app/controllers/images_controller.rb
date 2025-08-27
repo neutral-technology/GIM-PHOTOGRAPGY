66accf0e1c76d20271b00e22217c51acd0eab610
joskalenda <joskalenda3@gmail.com>
2025-08-27 09:45:08 +0200

    automate cover photo image on creation

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -5,8 +5,14 @@ class ImagesController < ApplicationController
   def create
     @album = current_user.albums.friendly.find(params[:album_id])
     if image_params[:photo].present?
+      new_images = []
       image_params[:photo].compact_blank.each do |p|
-        @album.images.create!(photo: p)
+        new_images << @album.images.create!(photo: p)
+      end
+      # New Logic: Automatically set a cover photo if one doesn't exist
+      if !@album.cover_photo.attached? && new_images.any?
+        random_image = new_images.sample
+        @album.cover_photo.attach(random_image.photo.blob)
       end
       redirect_to @album, notice: 'Images were successfully added.'
     else
