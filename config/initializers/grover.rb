81eba78bb99014ae4b27cf486c5d258f05dd0748
joskalenda <joskalenda3@gmail.com>
2026-01-21 17:53:35 +0200

    add grover for pdf download

---
+++ config/initializers/grover.rb
@@ -0,0 +1,14 @@
+Grover.configure do |config|
+  config.options = {
+    format: "A4",
+    margin: {
+      top: "10mm",
+      bottom: "10mm",
+      left: "10mm",
+      right: "10mm"
+    },
+    print_background: true,
+    # Indispensable pour que Puppeteer attende que les images et polices soient chargées
+    wait_until: 'networkidle2'
+  }
+end
