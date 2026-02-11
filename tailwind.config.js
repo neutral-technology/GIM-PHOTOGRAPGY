b8130c82bc7de1908e83216370c7dad5b528bf28
joskalenda <joskalenda3@gmail.com>
2026-02-12 01:18:43 +0200

    clean code,  change typos and static names

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*',
+    './app/views/brochures/editor/layouts/**/*.html.erb',
     './app/assets/stylesheets/**/*.css',
   ],
   safelist: [
