85ed147851b72d5ed483d8f422848ac67e00c3b6
joskalenda <joskalenda3@gmail.com>
2026-02-12 01:21:13 +0200

    add necessary codes

--- app/controllers/brochures_controller.rb
+++ app/controllers/brochures_controller.rb
@@ -4,27 +4,29 @@ class BrochuresController < ApplicationController
 
   def show
     @brochure = Brochure.find(params[:id])
-
-    # On charge le CSS pour tout le monde (HTML et PDF)
-    css_path = Rails.root.join('app/assets/builds/tailwind.css')
-    @css_content = File.exist?(css_path) ? File.read(css_path) : ''
-
+    @editing = false
     respond_to do |format|
       format.html # { render layout: 'pdf' }
       format.pdf do
-        html = render_to_string(template: 'brochures/show', layout: 'pdf', formats: [:html])
-        grover = Grover.new(html, display_url: 'http://localhost:3000',
-          print_background: true,
-          wait_until: 'networkidle2',
-          timeout: 0,
-          launch_args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-dev-shm-usage'
-          ]
+        html = render_to_string(
+          template: 'brochures/show',
+          layout: 'pdf',
+          formats: [:html]
         )
-        send_data grover.to_pdf, filename: 'gim.pdf', type: 'application/pdf', disposition: 'inline'
+        grover = Grover.new(
+          html,
+          display_url: request.base_url,
+          # print_background: true,
+          # wait_until: 'domcontentloaded',
+          timeout: 60_000,
+          launch_args: ['--no-sandbox', '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage'],
+
+        )
+        send_data grover.to_pdf,
+          filename: 'gim.pdf',
+          type: 'application/pdf',
+          disposition: 'inline'
       end
     end
   end
@@ -43,6 +45,7 @@ class BrochuresController < ApplicationController
     @brochure = Brochure
       .includes(pages: :blocks)
       .find(params[:id])
+    @editing = true
   end
 
   def update_theme
