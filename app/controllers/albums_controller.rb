28455a91f495814f6762501aaf68bc20b6a6d7ba
joskalenda <joskalenda3@gmail.com>
2026-03-28 04:21:58 +0200

    fix image error in album controlller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -10,20 +10,24 @@ class AlbumsController < ApplicationController
 
   def show
     authorize @album
+    @client = @album.client
 
     if @album.public?
       @images = @album.images.with_attached_photo.order(created_at: :desc).limit(6)    
     elsif user_signed_in?
       @image = @album.images.new
       @images = @album.images.with_attached_photo.order(created_at: :desc)
-      @client = @album.client
     else
       # Optional: allow public albums without login
       redirect_to new_user_session_path, notice: 'Connectez-vous pour accéder'
       return
     end
+     # 🔐 Who can upload (VERY IMPORTANT)
+    if policy(@album).update?
+      @image = @album.images.new
+      
+    end
   end
-
   def new
     @album = current_user.albums.new
     authorize @album
