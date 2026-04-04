abfe32562646a7d4c4a324e28aaa9a7863939d56
joskalenda <joskalenda3@gmail.com>
2026-04-04 16:59:01 +0200

    change to controller ->application

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,7 +1,6 @@
 // app/javascript/controllers/index.js
 // Import and register all your controllers from the importmap under controllers/*
-import { application } from "./application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import { application } from "controllers/application" // ✅ Better for Importmapsimport { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 eagerLoadControllersFrom("controllers", application)
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,7 +7,6 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin '@rails/ujs', to: 'https://ga.jspm.io/npm:@rails/ujs@7.1.3-4/app/assets/javascripts/rails-ujs.esm.js'
 pin 'sortablejs', to: 'https://ga.jspm.io/npm:sortablejs@1.15.0/modular/sortable.esm.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin "controllers/application", to: "application.js"
 
 # 🟢 ADD THIS LINE BELOW
 # config/importmap.rb
