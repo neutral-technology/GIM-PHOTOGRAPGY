32dec8fee0c81ab814b3b469cbfc3f4700b910d7
joskalenda <joskalenda3@gmail.com>
2026-04-06 16:00:52 +0200

    fix cusotm error and alpin

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,17 @@
 // app/javascript/application.js
 import "@hotwired/turbo-rails"
 import "controllers"
+// import "custom"
+
+import Alpine from "alpinejs"
+import persist from "@alpinejs/persist"
+import collapse from "@alpinejs/collapse"
+
+window.Alpine = Alpine
+
+Alpine.plugin(persist)
+Alpine.plugin(collapse)
+
 import "custom"
+
+Alpine.start()
