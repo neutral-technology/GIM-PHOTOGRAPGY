dacf61da0615669290b543d5181121ec4f4276a4
joskalenda <joskalenda3@gmail.com>
2026-04-06 21:56:45 +0200

    lean linters

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,10 +7,10 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin '@rails/ujs', to: 'https://ga.jspm.io/npm:@rails/ujs@7.1.3-4/app/assets/javascripts/rails-ujs.esm.js'
 pin 'sortablejs', to: 'https://ga.jspm.io/npm:sortablejs@1.15.0/modular/sortable.esm.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin "alpinejs", to: "https://unpkg.com/alpinejs@3.x.x/dist/module.esm.js"
-pin "@alpinejs/persist", to: "https://unpkg.com/@alpinejs/persist@3.x.x/dist/module.esm.js"
-pin "@alpinejs/collapse", to: "https://unpkg.com/@alpinejs/collapse@3.x.x/dist/module.esm.js"
-pin "custom" # @0.0.0
+pin 'alpinejs', to: 'https://unpkg.com/alpinejs@3.x.x/dist/module.esm.js'
+pin '@alpinejs/persist', to: 'https://unpkg.com/@alpinejs/persist@3.x.x/dist/module.esm.js'
+pin '@alpinejs/collapse', to: 'https://unpkg.com/@alpinejs/collapse@3.x.x/dist/module.esm.js'
+pin 'custom' # @0.0.0
 
 # 🟢 ADD THIS LINE BELOW
 # config/importmap.rb
