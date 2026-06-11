1d8d9138a3698a39c109953f82cf17039aa882a7
joskalenda <joskalenda3@gmail.com>
2026-06-11 21:24:24 +0200

    finish cover inv page

--- app/models/brochure_preset.rb
+++ app/models/brochure_preset.rb
@@ -94,14 +94,14 @@ class BrochurePreset < ApplicationRecord
     ],
 
     invitation_classic: [
-      { layout: 'inv_classic/i_1_hero', blocks: %w[image title paragraph] },
+      { layout: 'inv_classic/i_1_hero', blocks: %w[image title paragraph paragraph paragraph paragraph paragraph paragraph paragraph paragraph] },
       { layout: 'inv_classic/i_2_countdown', blocks: %w[title] },
       { layout: 'inv_classic/i_3_story', blocks: %w[title paragraph image] },
       { layout: 'inv_classic/i_4_event', blocks: %w[title paragraph] },
       { layout: 'inv_classic/i_5_reception', blocks: %w[title paragraph] },
       { layout: 'inv_classic/i_6_rsvp', blocks: %w[title paragraph] },
-      { layout: 'invitation/i_7_gallery', blocks: %w[image image image image] },
-      { layout: 'invitation/i_8_footer', blocks: %w[title paragraph] }
+      { layout: 'inv_classic/i_7_gallery', blocks: %w[image image image image] },
+      { layout: 'inv_classic/i_8_footer', blocks: %w[title paragraph] }
     ],
 
     invitation_lux: [
