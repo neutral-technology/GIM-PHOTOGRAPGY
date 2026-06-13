e461954f9a698f3c781113673712c9d5842a426a
joskalenda <joskalenda3@gmail.com>
2026-06-13 23:12:34 +0200

    create iinvitation and geuss controller

---
+++ app/controllers/invitation_guests_controller.rb
@@ -0,0 +1,36 @@
+class InvitationGuestsController < ApplicationController
+  layout 'default'
+  skip_after_action :verify_authorized
+  before_action :set_brochure
+
+  def create
+    @guest = @brochure.invitation_guests.create!(
+      guest_params
+    )
+    redirect_to album_gallery_path(@brochure.client.album),
+                notice: "Invité ajouté"
+  end
+
+  def destroy
+    guest = @brochure.invitation_guests.find(params[:id])
+    guest.destroy
+
+    redirect_to album_gallery_path(@brochure.client.album),
+            notice: "Invité Suprimé"
+  end
+
+  private
+
+  def set_brochure
+    @brochure = Brochure.find(params[:brochure_id])
+  end
+
+  def guest_params
+    params.require(:invitation_guest)
+      .permit(
+        :name,
+        :phone,
+        :table
+      )
+  end
+end
